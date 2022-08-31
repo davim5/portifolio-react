4c1834e9d313560825047df48be3107a1fc3e029
davi lima <davi.lima@g4flex.com.br>
2022-08-31 16:58:30 -0300

    Mostrando clima

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,21 @@ import { format } from 'date-fns';
 import {GithubLogo, LinkedinLogo } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { FooterContainer } from './Footer.styles';
+import api from '../services/api';
+
+interface IWeather {
+  name: string,
+  temperature:number,
+  sky:string,
+}
 
 export function Footer() {
     const [time,setTime] = useState(Date.now());
+    const [weather,setWeather] = useState<IWeather>({
+      name:'',
+      temperature:0,
+      sky:''
+    });
 
     useEffect(() => {
       let timer = setInterval(() => {
@@ -14,13 +26,28 @@ export function Footer() {
       return () => {clearInterval(timer)}
   },[time]);
 
+  useEffect(() => {
+    api.get('/city/?city=fortaleza').then(res => {
+      let formatedWeather:IWeather = {
+        name: res.data.name,
+        temperature: res.data.main.temp.toFixed(1),
+        sky: res.data.weather[0].description,
+      };
+      console.log(formatedWeather);
+
+      setWeather(formatedWeather);
+    });
+    console.log(navigator.geolocation.getCurrentPosition);
+
+  },[])
+  
   const formatedTime = format(time,'pppp');
 
     return (
         <FooterContainer>
           <div className='status'>
             <span>{formatedTime}</span>
-            <span>19°C • Rain • 1.54 km/h</span>
+            <span> {weather.name} • {weather.temperature}°C • {weather.sky}</span>
           </div>
           <div className='social'>
             <a href='https://github.com/davim5'><GithubLogo size={32} /></a>
